48142430c519430fb94faf18b935fe77b391ffcb
D-LUSiON <l.peikov@gmail.com>
2017-10-22 13:13:45 +0300

    FIX: Fixed initial installing; UPDATE: Some work on Dashboard; FIX: Showing of movie list when movies are loaded; UPDATE: More classes in spacing.css; UPDATE: Some styling;

--- src/app/modules/movies/movies-list/movies-list.component.ts
+++ src/app/modules/movies/movies-list/movies-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
 import { Movie } from '@app/models';
 import { MoviesService } from '@app/services';
 
@@ -14,20 +14,34 @@ export class MoviesListComponent implements OnInit {
     filtered_movies: Movie[] = [];
 
     constructor(
-        private moviesService: MoviesService
+        private moviesService: MoviesService,
+        private ngZone: NgZone,
     ) {
-        this.movies = this.moviesService.all_movies;
-        this.filtered_movies = this.movies.slice();
+        if (!this.moviesService.movies_loaded) {
+            this.moviesService.emmiter.asObservable().subscribe(() => {
+                this.ngZone.run(() => {
+                    this.movies = this.moviesService.all_movies;
+                    this.filtered_movies = this.movies.slice();
+                });
+            });
+        } else {
+            this.movies = this.moviesService.all_movies;
+            this.filtered_movies = this.movies.slice();
+        }
     }
 
-    ngOnInit() {}
+    ngOnInit() { }
 
     filterMovies(value) {
         this.filtered_movies = [];
         this.movies.forEach((movie, i) => {
-            if (movie.title.toLowerCase().indexOf(value.toLowerCase()) > -1)
+            if (movie.title.toLowerCase().indexOf(value.toLowerCase()) > -1 || movie.original_title.toLowerCase().indexOf(value.toLowerCase()) > -1)
                 this.filtered_movies.push(movie);
         });
+    }
+
+    removeMovie(event) {
+        event.preventDefault();
     }
 
 }
--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -8,86 +8,20 @@ import { NotificationsService } from './notifications.service';
 import { GenresService } from './genres.service';
 import { TMDBMovie } from '../models/tmdb-movie.model';
 import { ElectronService } from 'ngx-electron';
+import { Subject } from 'rxjs/Rx';
 
 @Injectable()
 export class MoviesService {
 
-    movies: Movie[] = [
-        // new Movie({
-        //     _id: '231tfag24',
-        //     title: 'Deadpool',
-        //     poster: 'assets/posters/deadpool--deadpool-free.jpg'
-        // }),
-        // new Movie({
-        //     _id: 'het23qg2w',
-        //     title: 'Elysium',
-        //     poster: 'assets/posters/elysium-movie-poster.jpg'
-        // }),
-        // new Movie({
-        //     _id: 'v2vtg35yhwsg',
-        //     title: 'John Wick',
-        //     poster: 'assets/posters/john-wick-movieposter.jpg'
-        // }),
-        // new Movie({
-        //     _id: 'g23gqaeg24',
-        //     title: 'John Wick - chapter 2',
-        //     poster: 'assets/posters/john-wick-chapter-2-poster.jpg'
-        // }),
-        // new Movie({
-        //     _id: 'iu4r5e',
-        //     title: 'Batman - the Dark Knight',
-        //     poster: 'assets/posters/batman--dark-knight--movie-poster.jpg'
-        // }),
-        // new Movie({
-        //     _id: '54hwreghw',
-        //     title: 'R.I.P.D.',
-        //     poster: 'assets/posters/ripd_xlg.jpg'
-        // }),
-        // new Movie({
-        //     _id: '54erhahbsd',
-        //     title: 'Star Trek',
-        //     poster: 'assets/posters/star-trek-movie-poster.jpg'
-        // }),
-        // new Movie({
-        //     _id: 'xcb3qhhada',
-        //     title: 'The Martian',
-        //     poster: 'assets/posters/martian_movie-poster-new.jpg'
-        // }),
-        // new Movie({
-        //     _id: '24egsag234h3',
-        //     title: 'Ghost in the Shell',
-        //     poster: 'assets/posters/ghost_in_the_shell_xlg.jpg'
-        // }),
-        // new Movie({
-        //     _id: 'g3erhbSg2q3',
-        //     title: 'Pirates of the Caribbean 5',
-        //     poster: 'assets/posters/Pirates-of-the-Caribbean-5-Movie-Poster.jpg'
-        // }),
-        // new Movie({
-        //     _id: 'sgwahgq3h',
-        //     title: 'The Avengers',
-        //     poster: 'assets/posters/newavengersposter.jpg'
-        // }),
-        // new Movie({
-        //     _id: 'sadg234tg2qg',
-        //     title: 'Alien - Covenant',
-        //     poster: 'assets/posters/alien_covenant_ver4.jpg'
-        // }),
-        // new Movie({
-        //     _id: 'gbwegwasghwea',
-        //     title: 'Tarzan',
-        //     poster: 'assets/posters/tarzan_ver3_xlg.jpg'
-        // }),
-        // new Movie({
-        //     _id: 'gswegwSgewg',
-        //     title: 'Hunger Games - Mockingjay pt.1',
-        //     poster: 'assets/posters/mocking-jay-movieposter.jpg'
-        // }),
-    ];
+    movies: Movie[] = [];
 
     private tmtb_api_key: string = '';
 
     private language = 'en';
+
+    emmiter: Subject<any> = new Subject();
+
+    movies_loaded: boolean = false;
 
     constructor(
         private electron: ElectronService,
@@ -109,12 +43,14 @@ export class MoviesService {
     }
 
     private _getMovies() {
-        // TODO: Event emmiter for loaded movies
         this.electron.ipcRenderer.once('Movies:get:response', (event, response: Movie[]) => {
             this.movies = response.map((x) => {
                 return new Movie(x);
             });
-            console.log(this.movies);
+
+            this.movies_loaded = true;
+
+            this.emmiter.next(this.movies_loaded);
         });
 
         this.electron.ipcRenderer.send('Movies:get');
